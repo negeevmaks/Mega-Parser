a6899f29daae78d8fee069e6e7c90beda79233a0
negeevmaks <93725454+negeevmaks@users.noreply.github.com>
2023-08-14 09:14:14 +0300

    Add files via upload

---
+++ Mash/autoline.py
@@ -0,0 +1,228 @@
+import requests
+import ui
+from bs4 import BeautifulSoup
+import openpyxl
+import os
+
+base_url = 'https://autoline.ua/'
+
+def get_category():
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    category = []
+
+    ads = soup.find_all('a', {'class': 'item'})
+
+    for ad in ads:
+        if 'https://autoline.ua' in ad.get('href'):
+            category.append(ad.get('href').replace('https://autoline.ua/-/', ''))
+
+    return category
+
+def pages(link):
+    resuk = []
+
+    resuk.append(f'https://autoline.ua/-/{link}')
+
+    return set(list(resuk))
+
+def get_links(category, cares, num_f = 0):
+    returned = []
+    reh = []
+    itemed = []
+
+    for cat in category:
+        link = set(list(pages(cat)))
+
+        for li in link:
+            response = requests.get(li)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            result = soup.find_all('div', {'class': 'subitem'})
+
+            for res in result:
+                rem = res.find('a')
+
+                reh.append(rem.get('href'))
+
+            for i in range(0, num_f):
+                reh.pop(0)
+
+            for res in reh:
+                response = requests.get(res)
+                soup = BeautifulSoup(response.text, 'html.parser')
+
+                mogaz = soup.find_all('div', {'class': 'item sales-list-item with-widget-price-extension with-widget-photo-widget item-active ecommerce-item'})
+
+                for item in mogaz:
+                    rek = item.find('a')
+                    reg = rek.get('href')
+
+                    itemed.append(reg)
+                    print(reg)
+                    print(len(itemed))
+
+                    if len(itemed) >= cares:
+                        return itemed
+
+    return returned
+
+def check_model_exists(url, ws):
+    for row in ws.iter_rows(min_row=2):  # Починаємо з другого рядка (першій рядок містить заголовки)
+        cell_value = row[0].value  # Перевіряємо значення в першому стовпці (A стовпець для URL)
+
+        if url in cell_value:
+            return True  # Запис з вказаним URL знайдено
+
+    return False  # Запис з вказаним URL не знайден
+
+def start_parsing(category, pric, cares, result_folder):
+    
+    def create_workbook(path):
+        if not os.path.isfile(path):
+            workbook = openpyxl.Workbook()
+            workbook.save(path)
+
+    workbook_path = f'{result_folder}/autoline.xlsx'
+    
+    create_workbook(workbook_path)
+
+    try:
+        wb = openpyxl.load_workbook(workbook_path)
+    except openpyxl.utils.exceptions.InvalidFileException:
+        os.remove(workbook_path)  # Delete the corrupted file
+        create_workbook(workbook_path)  # Create a new workbook
+        wb = openpyxl.load_workbook(workbook_path)
+        
+    ws = wb.active
+
+    links = get_links(category, cares)
+
+    end = []
+    ens = 0
+    last_row = 2
+    num_f = 1
+
+    for link in links:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        price = soup.find('div', {'class': 'value'}).text.replace('   ', '').replace('\n', '').replace('  ', '')
+
+
+
+        if price != 'за запитом':
+            try:
+                if 'грн' in price:
+                    if 'грн/зм' in price:
+                        pride = int(price[:-9].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                    else:
+                        pride = int(price[:-3].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                elif '—(аукціон' in price:
+                    pride = int(price[:-9].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                else:
+                    pride = int(price[:-1].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+            except:
+                pride = 0
+
+            if pric[0] < pride and pride < pric[1]:
+                if soup.find('span', {'class': 'loc-country'}).text == 'Україна':
+                    if check_model_exists(link, ws) != True:
+                        ws[f'A{last_row}'] = f'URL: {link}'
+                        ws[f'B{last_row}'] = f"Model: {(soup.find('h1', {'class': 'sf-title'}).text)}"
+                        ws[f'C{last_row}'] = f'Price: {price}'
+                        ws[f'D{last_row}'] = f"Number: {' '.join(text.text.replace('        ', '') for text in soup.find_all('a', {'class': 'full-number'}))}"
+
+                        wb.save(f'{result_folder}/autoline.xlsx')
+
+                        last_row += 1
+                        ens += 1
+
+                        ui.ui_update(cares, ens)
+
+        else:
+            if soup.find('span', {'class': 'loc-country'}).text == 'Україна':
+                if check_model_exists(link, ws) != True:
+                    ws[f'A{last_row}'] = f'URL: {link}'
+                    ws[f'B{last_row}'] = f"Model: {(soup.find('h1', {'class': 'sf-title'}).text)}"
+                    ws[f'C{last_row}'] = f'Price: {price}'
+                    ws[f'D{last_row}'] = f"Number: {' '.join(text.text.replace('        ', '') for text in soup.find_all('a', {'class': 'full-number'}))}"
+
+                    wb.save(f'{result_folder}/autoline.xlsx')
+
+                    last_row += 1
+                    ens += 1
+
+                    ui.ui_update(cares, ens)
+
+    while True:
+        num_f += 1
+        if ens < cares:
+            try:
+                links = get_links([*(category + f'?page={num_f}' for category in category)], cares - ens - 1, num_f)
+                for link in links:
+                    response = requests.get(link)
+                    soup = BeautifulSoup(response.text, 'html.parser')
+
+                    price = soup.find('div', {'class': 'value'}).text.replace('   ', '').replace('\n', '').replace('  ', '')
+
+
+
+                    if price != 'за запитом':
+                        try:
+                            if 'грн' in price:
+                                if 'грн/зм' in price:
+                                    pride = int(price[:-9].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                                else:
+                                    pride = int(price[:-3].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                            elif '—(аукціон' in price:
+                                pride = int(price[:-9].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                            else:
+                                pride = int(price[:-1].replace(" ", "").replace("\xa0", "").replace(',', ''))
+
+                        except:
+                            pride = 0
+
+                        if pric[0] < pride and pride < pric[1]:
+                            if soup.find('span', {'class': 'loc-country'}).text == 'Україна':
+                                if check_model_exists(link, ws) != True:
+                                    ws[f'A{last_row}'] = f'URL: {link}'
+                                    ws[f'B{last_row}'] = f"Model: {(soup.find('h1', {'class': 'sf-title'}).text)}"
+                                    ws[f'C{last_row}'] = f'Price: {price}'
+                                    ws[f'D{last_row}'] = f"Number: {' '.join(text.text.replace('        ', '') for text in soup.find_all('a', {'class': 'full-number'}))}"
+
+                                    wb.save(f'{result_folder}/autoline.xlsx')
+
+                                    last_row += 1
+                                    ens += 1
+
+                                    ui.ui_update(cares, ens)
+
+                    else:
+                        if soup.find('span', {'class': 'loc-country'}).text == 'Україна':
+                            if check_model_exists(link, ws) != True:
+                                ws[f'A{last_row}'] = f'URL: {link}'
+                                ws[f'B{last_row}'] = f"Model: {(soup.find('h1', {'class': 'sf-title'}).text)}"
+                                ws[f'C{last_row}'] = f'Price: {price}'
+                                ws[f'D{last_row}'] = f"Number: {' '.join(text.text.replace('        ', '') for text in soup.find_all('a', {'class': 'full-number'}))}"
+
+                                wb.save(f'{result_folder}/autoline.xlsx')
+                                last_row += 1
+                                ens += 1
+
+                                ui.ui_update(cares, ens)
+            except:
+                break
+
+        else:
+            break
+
+    ui.ui_update(1, 1)
---
+++ frames.py
@@ -0,0 +1,198 @@
+import customtkinter as ctk
+from spinbox import Spinbox
+from math import ceil
+from os import getcwd
+from time import sleep
+from datetime import datetime
+options = {
+    "AUTO.RIA": ['options.AUTO.RIA.1', 'options.AUTO.RIA.2', 'options.AUTO.RIA.3', 
+                'options.AUTO.RIA.4', 'options.AUTO.RIA.5', 'options.AUTO.RIA.6', 
+                'options.AUTO.RIA.7', 'options.AUTO.RIA.8', 'options.AUTO.RIA.9'], 
+    "Machineryline": ['options.Machineryline.1', 'options.Machineryline.2', 'options.Machineryline.3'], 
+    "Autoline": ['options.Autoline.1', 'options.Autoline.2', 'options.Autoline.3'], 
+    "Agriline": ['options.Agriline.1', 'options.Agriline.2', 'options.Agriline.3'], 
+}
+def round_up(number, multiple):
+    return int(ceil(number / multiple) * multiple)
+
+class SiteChooseFrame(ctk.CTkFrame):
+    def __init__(self, master, title, values, checkbox_frame):
+        super().__init__(master)
+        self.values = values
+        self.title = title
+        self.radiobuttons = []
+        self.variable = ctk.StringVar(value="")
+
+        self.title = ctk.CTkLabel(self, text=self.title, 
+                                fg_color=("#C0C0C0","gray30"), corner_radius=6)
+        self.title.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="ew", columnspan=2)
+
+        for i, value in enumerate(self.values):
+            radiobutton = ctk.CTkRadioButton(self, text=value, value=value, variable=self.variable,
+                                            command=lambda val=value: self.update_checkbox_frame(val, checkbox_frame))
+            radiobutton.grid(row=1, column=i, padx=10, pady=(10, 10), sticky="w")
+            self.radiobuttons.append(radiobutton)
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
+
+    def update_checkbox_frame(self, value, checkbox_frame):
+        import main
+        checkbox_frame.set(main.checks(value))
+
+class MyScrollableCheckboxFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master, label_text=title)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.checkboxes = []
+        self.create_label()
+
+    def create_label(self):
+        self.label = ctk.CTkLabel(self, text="Виберіть сайт")
+        self.label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.is_label = True
+
+    def create_checkboxes(self):
+        if self.is_label:
+            self.label.grid_forget()
+            self.label.destroy()
+            self.is_label = False
+        for i, value in enumerate(self.values):
+            checkbox = ctk.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i+1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def set(self, new_values):
+        # Clear existing checkboxes
+        for checkbox in self.checkboxes:
+            checkbox.grid_forget()
+            checkbox.destroy()
+        self.checkboxes = []
+
+        # Set new checkboxes
+        self.values = new_values
+        self.create_checkboxes()
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+class AmountChooseFrame(ctk.CTkFrame):
+    def __init__(self, master, title, min_val, max_val):
+        super().__init__(master)
+        self.title = title
+        self.max = max_val
+        self.min = min_val
+        
+        self.title_label = ctk.CTkLabel(self, text=self.title,
+                                        fg_color=("#C0C0C0", "gray30"), corner_radius=6)
+        self.title_label.grid(row=0, column=0, 
+                            padx=10, pady=10, 
+                            sticky="w")
+
+        self.amount = ctk.CTkSlider(self, from_=self.min, to=self.max,
+                                    command=self.slider_event)
+        self.amount.set(30)
+        self.amount.grid(row=1, column=0, 
+                        padx=10, pady=10, 
+                        sticky="ew", columnspan=3)
+
+        self.amount_label = ctk.CTkLabel(self, text=f"Поточне значення: {int(self.amount.get())}", corner_radius=6)
+        self.amount_label.grid(row=0, column=2, 
+                            padx=10, pady=10, 
+                            sticky="w")
+
+        self.grid_columnconfigure(1, weight=1)
+
+    def slider_event(self, value):
+        rounded_value = round_up(value, 10)
+        self.amount_label.configure(text=f"Кількість машин: {rounded_value}")
+    
+    def get(self):
+        return round_up(self.amount.get(), 10)
+
+class YearChooseFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        year = datetime.now().year
+        
+        self.year_main_title = ctk.CTkLabel(self, text="Рік", 
+                                            fg_color=("#C0C0C0","gray30"), 
+                                            corner_radius=6)
+        self.year_main_title.grid(row=0, column=0, 
+                                padx=10, pady=10, 
+                                sticky="ew", rowspan=2)
+        
+        
+        self.min_year_title = ctk.CTkLabel(self, text="Від:", corner_radius=6)
+        self.min_year_title.grid(row=0, column=1, 
+                                padx=10, pady=10, 
+                                sticky="ew")
+        
+        self.min_year_spinbox = Spinbox(self, width=150, 
+                                        min_value=0,
+                                        max_value=year, 
+                                        default_value = year)
+        self.min_year_spinbox.grid(row=0, column=2)
+        
+        self.max_year_title = ctk.CTkLabel(self, text="До:", corner_radius=6)
+        self.max_year_title.grid(row=1, column=1, 
+                                padx=10, pady=10, 
+                                sticky="ew")
+        
+        self.max_year_spinbox = Spinbox(self, width=150, 
+                                        min_value=0, 
+                                        max_value=year, 
+                                        default_value = year)
+        self.max_year_spinbox.grid(row=1, column=2, 
+                                padx=10)
+    
+    def get_min(self):
+        return self.min_year_spinbox.get()
+    def get_max(self):
+        return self.max_year_spinbox.get()
+
+class PriceChooseFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.price_main_title =  ctk.CTkLabel(self, text="Ціна", 
+                                            fg_color=("#C0C0C0","gray30"), 
+                                            corner_radius=6)
+        self.price_main_title.grid(row=0, column=0, 
+                                padx=10, pady=10, 
+                                sticky="ew", rowspan=2)
+        
+        
+        self.min_price_title = ctk.CTkLabel(self, text="Від:", corner_radius=6)
+        self.min_price_title.grid(row=0, column=1, 
+                                padx=10, pady=10, 
+                                sticky="ew")
+        
+        self.min_price_spinbox = Spinbox(self, width=150, 
+                                        min_value=0, 
+                                        default_value = 0)
+        self.min_price_spinbox.grid(row=0, column=2)
+        
+        self.max_price_title = ctk.CTkLabel(self, text="До:", corner_radius=6)
+        self.max_price_title.grid(row=1, column=1, 
+                                padx=10, pady=10, 
+                                sticky="ew")
+        
+        self.max_price_spinbox = Spinbox(self, width=150, 
+                                        min_value=0, 
+                                        default_value = 0)
+        self.max_price_spinbox.grid(row=1, column=2, 
+                                    padx=10)
+    
+    def get_min(self):
+        return self.min_price_spinbox.get()
+    def get_max(self):
+        return self.max_price_spinbox.get()
